21d0cd1398924a61687863aab338986ed79f6657
scarv <fernando.carvalho@lcsistemas.com.br>
2024-11-07 17:25:03 -0300

    fix: fixed login page images

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,26 +1,33 @@
 import Image from "next/image";
+import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
 
 const LoginPage = () => {
   return (
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDA */}
-      <div className="flex flex-col h-full max-w-[500px] p-8 mx-auto">
+      <div className="flex flex-col h-full max-w-[550px] p-8 mx-auto">
         <Image
-          src="/public/logo.png"
+          src={"Logo.svg"}
           alt="Finance AI"
           width={173}
           height={39}
+          className="mb-8"
         />
         <h1 className="text-4xl font-bold">Bem-vindo</h1>
         <p>
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, e oferecer insights personalizados, facilitando o controle do seu orçamento.
         </p>
+        <Button variant={"outline"} className="mt-8">
+          <LogInIcon className="mr-2" />
+          Fazer login ou criar conta
+        </Button>
       </div>
 
       {/* DIREITA */}
       <div className="relative h-full w-full">
         <Image
-          src="/public/login.png"
+          src={"/Login.png"}
           alt="Faça login"
           fill
           className="object-cover" />
