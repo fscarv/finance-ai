0462dfedb7b3025762d1ea4dc7f18297bf372224
scarv <fernando.carvalho@lcsistemas.com.br>
2024-11-22 09:50:16 -0300

    feat: ensure transaction can be updated

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -6,6 +6,7 @@ import TransactionTypeBadge from "../_componentes/type-badge";
 import { Button } from "@/app/_components/ui/button";
 import { SquarePenIcon, TrashIcon } from "lucide-react";
 import { TRANSACTION_CATEGORY_LABELS, TRANSACTION_PAYMENT_METHOD_LABELS } from "@/app/_constants/transactions";
+import EditTransactionButton from "../_componentes/edit-transaction-button";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -22,19 +23,19 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
-    cell: ({ row: { original: transaction } }) => 
+    cell: ({ row: { original: transaction } }) =>
       TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Forma de pagamento",
-    cell: ({ row: { original: transaction } }) => 
+    cell: ({ row: { original: transaction } }) =>
       TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Data",
-    cell: ({ row: { original: transaction } }) => 
+    cell: ({ row: { original: transaction } }) =>
       new Date(transaction.date).toLocaleDateString("pt-BR", {
         day: "2-digit",
         month: "long",
@@ -44,7 +45,7 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "amount",
     header: "Valor",
-    cell: ({ row: { original: transaction } }) => 
+    cell: ({ row: { original: transaction } }) =>
       new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL",
@@ -53,13 +54,15 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "action",
     header: "",
-    cell: () => {
+    cell: ({ row: { original: transaction } }) => {
       return (
         <div className="space-x-1">
-          <Button variant={"ghost"} size={"icon"} className="text-muted-foreground">
-            <SquarePenIcon />
-          </Button>
-          <Button variant={"ghost"} size={"icon"} className="text-muted-foreground">
+          <EditTransactionButton transaction={transaction} />
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            className="text-muted-foreground"
+          >
             <TrashIcon />
           </Button>
         </div>
