93f49c588f475d69e688646bdf10a5e896ea9c17
scarv <fernando.carvalho@lcsistemas.com.br>
2024-12-04 10:54:42 -0300

    fix: correction of value on home page

--- app/(home)/_components/percentage-item.tsx
+++ app/(home)/_components/percentage-item.tsx
@@ -10,15 +10,15 @@ const PercentageItem = ({ icon, title, value }: PercentageItemProps) => {
   return (
     <div className="flex justify-between items-center">
       {/* ICONE */}
-      <div className="flex items-center gap-3">
-        <div className="bg-white bg-opacity-[3%] rounded-lg p-2">
+      <div className="flex items-center gap-2">
+        <div className="bg-white bg-opacity-[3%] rounded-lg">
           {icon}
         </div>
         <p className="text-sm text-muted-foreground">{title}</p>
       </div>
       {/* PORCENTAGEM */}
-      <p className="text-sm font-bold">
-        {value}%
+      <p className="text-sm font-bold ml-2">
+        {isNaN(value) ? 0 : value}%
       </p>
     </div>
   );
